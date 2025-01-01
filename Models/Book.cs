72f56311cae7e136f407fc45d33a8ef8130b180e
Allan Rizza <allanrizza.dev@gmail.com>
2025-01-01 20:12:45 -0300

    creating book and category models

---
+++ Models/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace personallib_be.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public required string Title { get; set; }
+        public ICollection<Category> Categories { get; } = new List<Category>();
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace personallib_be.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
